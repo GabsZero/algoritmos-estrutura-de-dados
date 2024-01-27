3a6e559f8ab719b2347879054e90bd7f8fd484e7
Johnny <joaogbdesouza@gmail.com>
2024-01-27 14:40:29 -0300

    mais exemplos de código

--- Big-O-Notation/bigo.go
+++ Big-O-Notation/bigo.go
@@ -9,9 +9,21 @@ import (
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	listaAleatoria := rand.Perm(1000)
+	// listaAleatoria := rand.Perm(100000)
+	lutadores := []string{"ken", "chunli", "blanka", "sagat", "bison", "vega", "balrog", "dhalsin", "akuma", "ryu"}
+	// percorrerLista(listaAleatoria)
+	// calculaPrimeiroNumero(listaAleatoria)
+	encontraRyu(lutadores)
+}
 
-	percorrerLista(listaAleatoria)
+func encontraRyu(lutadores []string) {
+	for _, lutador := range lutadores {
+		fmt.Println("Tentando encontrar o ryu")
+		if lutador == "ryu" {
+			fmt.Println("Encontrei o ryu!")
+			break
+		}
+	}
 }
 
 func percorrerLista(array []int) {
@@ -23,4 +35,13 @@ func percorrerLista(array []int) {
 	}
 	tempo := time.Since(inicio)
 	log.Printf("Percorrer a lista levou %s", tempo)
+}
+
+func calculaPrimeiroNumero(array []int) {
+	inicio := time.Now()
+	if array[0] > 0 {
+		fmt.Println("Número é maior que 0!")
+	}
+	tempo := time.Since(inicio)
+	log.Printf("Calcular o primeiro número levou %s", tempo)
 }
